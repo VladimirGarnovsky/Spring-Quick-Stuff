7357c495ebfcd35fd1d9608470181bd78bc0d006
Vladimir <garnovsky.vladimir@gmail.com>
2018-12-05 20:22:41 +0100

    Spring XML Properties ex
    How to use properties with spring

---
+++ spring_sample_properties/src/main/java/com/pluralsight/repository/HibernateCustomerRepositoryImpl.java
@@ -0,0 +1,27 @@
+package com.pluralsight.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.pluralsight.model.Customer;
+
+public class HibernateCustomerRepositoryImpl implements CustomerRepository {
+	
+	private String dbUsername;
+	
+	public void setDbUsername(String dbUsername) {
+		this.dbUsername = dbUsername;
+	}
+	@Override
+	public List<Customer> findAll(){
+		System.out.println(dbUsername);
+		List<Customer> customers = new ArrayList<>();
+		Customer customer = new Customer();
+		customer.setFirstname("Vladimir");
+		customer.setLastname("Garnovski");
+		
+		customers.add(customer);
+		
+		return customers;
+	}
+}
